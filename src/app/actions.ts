0db9281f6cd7c215232f4535c2828f23a247e9bb
Hamdhan-Naheem <naheemhamdhan77@gmail.com>
2024-11-01 00:41:35 +0530

    Completed task two

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -5,5 +5,16 @@ import { redirect } from "next/navigation";
 
 export async function redirectToDashboard(formData: FormData) {
   // CODE FOR TASK 2.4 -----------------------------------------
+  const name = formData.get("name") as string;
+
+  if (!name || name.trim() === "") {
+    return "Hey, your name is missing!";
+  }
+
+  const cleanedName = name.trim().toLowerCase().trim();
+
+  cookies().set("gr-name", cleanedName);
+
+  redirect("/dashboard");
   // END OF CODE FOR TASK 2.4 ----------------------------------
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,42 @@ export default function Page() {
     <main className="bg-teal-100 flex flex-col h-screen">
       <Watermark />
       {error && <Error message={error} />}
-      {/* // CODE FOR TASK 2.2 -----------------------------------------
-      // END OF CODE FOR TASK 2.2 ---------------------------------- */}
+      {/* // CODE FOR TASK 2.2 ----------------------------------------- */}
+      <section className="flex-1 flex items-center justify-center p-4 z-10">
+        <div>
+          <p className="animate-pulse bg-gradient-to-r from-teal-500 via-teal-400 to-teal-300 bg-clip-text text-transparent text-3xl">
+            Welcome to
+          </p>
+          <p className="animate-pulse bg-gradient-to-r from-teal-500 via-teal-400 to-teal-300 bg-clip-text text-transparent text-6xl font-bold mt-2">
+            Graph Renderer.
+          </p>
+          <form
+            action={async (formData) => {
+              const error = await redirectToDashboard(formData);
+              if (error) {
+                setError(error);
+              }
+            }}
+          >
+            <div className="mt-8 border-2 p-5 bg-slate-50 border-gray-300">
+              <label htmlFor="name" className="block mb-2 text-gray-600">
+                What's your name?
+              </label>
+              <div className="flex gap-2 [&_*:focus]:outline-none [&_*:focus]:ring-2 [&_*:focus]:ring-black [&_*:focus]:border-transparent">
+                <Input
+                  id="name"
+                  type="text"
+                  placeholder="e.g. John Appleseed"
+                />
+                <Button type="submit" theme="primary">
+                  Continue
+                </Button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
+      {/* // END OF CODE FOR TASK 2.2 ---------------------------------- */}
     </main>
   );
 }
